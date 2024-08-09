d1f5d09fb1d582bdd8353f39c897c931da17c3d5
jeonDev <jjhwhdgus0727@gmail.com>
2024-08-09 21:37:47 +0900

    시작

---
+++ src/main/kotlin/com/study/kopring/ExKopringApplication.kt
@@ -0,0 +1,11 @@
+package com.study.kopring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExKopringApplication
+
+fun main(args: Array<String>) {
+    runApplication<ExKopringApplication>(*args)
+}
