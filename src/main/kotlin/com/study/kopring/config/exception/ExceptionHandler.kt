80c7164cf5273851b36e4fa4cdabfbf2c91ba603
jeonDev <jjhwhdgus0727@gmail.com>
2024-08-21 23:10:15 +0900

    refactor : global Exception logging

--- src/main/kotlin/com/study/kopring/config/exception/ExceptionHandler.kt
+++ src/main/kotlin/com/study/kopring/config/exception/ExceptionHandler.kt
@@ -1,15 +1,20 @@
 package com.study.kopring.config.exception
 
+import lombok.extern.slf4j.Slf4j
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
+@Slf4j
 @RestControllerAdvice
 class ExceptionHandler {
+    private val log = LoggerFactory.getLogger(ExceptionHandler::class.java)
 
     @ExceptionHandler(Exception::class)
     protected fun handleException(e: Exception) : ResponseEntity<ErrorResponse> {
+        log.error("Exception Log : {}", e.message, e)
         return ResponseEntity
             .status(HttpStatus.OK)
             .body(ErrorResponse())
