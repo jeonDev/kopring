220fb34b70fe35dbe61318e87ef87df92884847c
jeonDev <jjhwhdgus0727@gmail.com>
2024-08-24 21:51:40 +0900

    feat : encoder

--- src/main/kotlin/com/study/kopring/member/MemberServiceImpl.kt
+++ src/main/kotlin/com/study/kopring/member/MemberServiceImpl.kt
@@ -4,16 +4,21 @@ import com.study.kopring.member.entity.Member
 import com.study.kopring.member.repository.MemberRepository
 import com.study.kopring.member.vo.request.PMemberAdd
 import com.study.kopring.member.vo.response.RMemberAdd
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
 class MemberServiceImpl(
-    private val memberRepository: MemberRepository
+    private val memberRepository: MemberRepository,
+    private val encoder:PasswordEncoder
 ): MemberService {
 
     override fun add(pMemberAdd: PMemberAdd): RMemberAdd {
-        val entity:Member = pMemberAdd.toEntity(pMemberAdd.getPassword())
-        memberRepository.save(entity)
+        val entity:Member = memberRepository.save(
+            pMemberAdd.toEntity(
+                encoder.encode(pMemberAdd.password)
+            )
+        )
         return RMemberAdd(
             id = entity.memberId,
             name = entity.name
--- src/main/kotlin/com/study/kopring/member/vo/request/PMemberAdd.kt
+++ src/main/kotlin/com/study/kopring/member/vo/request/PMemberAdd.kt
@@ -3,14 +3,10 @@ package com.study.kopring.member.vo.request
 import com.study.kopring.member.entity.Member
 
 class PMemberAdd (
-    private var id:String,
-    private var password: String,
-    private var name: String
+    var id:String,
+    var password: String,
+    var name: String
 ){
-
-    fun getPassword():String {
-        return password;
-    }
 
     fun toEntity(encPassword: String) :Member {
         return Member(
